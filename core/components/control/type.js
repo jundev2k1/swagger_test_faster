2c3fb271894cb1dbf313e679cde7792fe5535158
JunDev <jun.dev2001@gmail.com>
2025-07-25 08:38:12 +0700

    Merge from sub branch

---
+++ core/components/control/type.js
@@ -0,0 +1,21 @@
+// ================================================
+// File: type.js
+// Description: Contains types of control
+// Copyright (c) 2025. Jun Dev
+// ================================================
+
+/**
+ * @typedef {Object} SegmentedInputProps
+ * @property {string} name - The name of the pill combobox.
+ * @property {SegmentedInputOption[]} options - The list of options.
+ * @property {string} selectedValue - The value of the selected option.
+ * @property {boolean} isCheckbox - Whether the pill combobox is a checkbox or not.
+ * @property {boolean} isPill - Whether the pill combobox is a pill or not.
+ */
+
+/**
+ * @typedef {Object} SegmentedInputOption
+ * @property {string} label - The label of the option.
+ * @property {string} value - The value of the option.
+ * @property {boolean} disabled - Whether the option is disabled or not.
+ */
